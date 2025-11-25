29cb5849cad7e032f5f8b152760aefc723cc261b
Aditya Sunil Nair <adityasunilnair227@gmail.com>
2025-11-25 09:19:29 +0000

    changed image size from 512 -> 384px

--- transforms.py
+++ transforms.py
@@ -4,7 +4,7 @@ def get_transforms(split):
     if split == 'train':
         return albumentations.Compose([
             albumentations.Rotate(p=0.5),
-            albumentations.Resize(512,512),
+            albumentations.Resize(384,384),
             albumentations.RandomBrightnessContrast(p=0.5),
             albumentations.HorizontalFlip(p=0.5),
             albumentations.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5)),
@@ -12,7 +12,7 @@ def get_transforms(split):
         ])
     else:
         return albumentations.Compose([
-            albumentations.Resize(512,512),
+            albumentations.Resize(384,384),
             albumentations.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5)),
             albumentations.ToTensorV2()
         ])
